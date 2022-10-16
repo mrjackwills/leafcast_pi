b36b553cfef332b7fa339e7efd472fe51e456dd5
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-10-16 18:01:36 +0000

    chore: create_release v0.1.2

--- create_release.sh
+++ create_release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # rust create_release
-# v0.1.0
+# v0.1.2
 
-PACKAGE_NAME='leafcast'
 STAR_LINE='****************************************'
 CWD=$(pwd)
 
@@ -13,17 +12,11 @@ YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
 RESET='\033[0m'
 
-
 # $1 string - error message
 error_close() {
 	echo -e "\n${RED}ERROR - EXITED: ${YELLOW}$1${RESET}\n";
 	exit 1
 }
-
-if [ -z "$PACKAGE_NAME" ]
-then
-	error_close "No package name"
-fi
 
 # $1 string - question to ask
 ask_yn () {
@@ -115,7 +108,7 @@ update_release_body_and_changelog () {
 
 	# Update changelog to add links to closed issues - comma included!
 	# "closes [#1]," -> "closes [#1](https:/www.../issues/1),""
-	sed -i -r -E "s=closes \[#([0-9]+)\],=closes [#\1](${GIT_REPO_URL}/issues/\1),=g" ./CHANGELOG.md
+	sed -i -r -E "s=closes \#([0-9]+)\,=closes [#\1](${GIT_REPO_URL}/issues/\1),=g" ./CHANGELOG.md
 }
 
 # update version in cargo.toml, to match selected current version
@@ -220,7 +213,7 @@ release_flow() {
 	release_continue "git add ."
 	git add .
 
-	release_continue "git commit -mg \"chore: release \"${NEW_TAG_WITH_V}\""
+	release_continue "git commit -m \"chore: release \"${NEW_TAG_WITH_V}\""
 	git commit -m "chore: release ${NEW_TAG_WITH_V}"
 
 	release_continue "git checkout main"
@@ -238,7 +231,7 @@ release_flow() {
 	release_continue "git checkout dev"
 	git checkout dev
 
-	release_continue "git merge --no-ff main -m 'chore: merge main into dev'"
+	release_continue "git merge --no-ff main -m \"chore: merge main into dev\""
 	git merge --no-ff main -m 'chore: merge main into dev'
 
 	release_continue "git push origin dev"
