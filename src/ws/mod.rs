d5bb16ba4b0c349d0bfc76f62aa491764cd3dbbf
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-04-07 08:24:52 +0000

    chore: Rust 1.86.0 linting

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -31,7 +31,7 @@ impl AutoClose {
     fn on_ping(&mut self, ws_sender: &WSSender) {
         if let Some(handle) = self.0.as_ref() {
             handle.abort();
-        };
+        }
         let ws_sender = C!(ws_sender);
         self.0 = Some(tokio::spawn(async move {
             tokio::time::sleep(std::time::Duration::from_secs(40)).await;
@@ -58,7 +58,7 @@ async fn incoming_ws_message(mut reader: WSReader, ws_sender: WSSender) {
                 break;
             }
             _ => (),
-        };
+        }
     }
     info!("incoming_ws_message done");
 }
